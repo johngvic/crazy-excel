e36fcb00ab4504cfeb2d2ea1c7c9abc4c65d391f
Joao D <j17duarte@hotmail.com>
2023-04-30 02:04:58 -0300

    fixing indexes mapping

--- main.js
+++ main.js
@@ -9,22 +9,22 @@ const invoke = async () => {
         return;
     }
 
-    console.log('Start processing...');
+    console.log(`Found ${encounteredFile}. Start processing...`);
 
     const inputWorkbook = new xlsx.readFile(encounteredFile, { dense: true });
     const outputWorkbook = xlsx.utils.book_new();
     const sheets = inputWorkbook.SheetNames;
 
     for (const sheet of sheets) {
-        console.log(`Processing tab sheet "${sheet}"`);
+        console.log(`\nProcessing tab sheet "${sheet}"`);
         const rows = xlsx.utils.sheet_to_json(inputWorkbook.Sheets[sheet], { header: 1 });
         const rawObjs = [];
 
         rows.forEach((row, rowIndex) => {
-            const auxObj = {}
-    
+            const auxObj = {};
+
             row.forEach((element, elementIndex) => {
-                element != null ? auxObj[elementIndex] = element : auxObj[elementIndex] = ''
+                element != null ? auxObj[elementIndex] = element : auxObj[elementIndex] = '';
     
                 if (rowIndex > 1 && auxObj[elementIndex] == '' && elementIndex > 0) {
                     auxObj[elementIndex] = '-'
@@ -32,31 +32,33 @@ const invoke = async () => {
             })
     
             if (rowIndex == 1) {
-                auxObj[67] = ''
-                auxObj[68] = 'Ano'
-                auxObj[69] = 'Venda/Devolução'
-                auxObj[70] = 'Classificação'
-                auxObj[71] = 'Valor Isenção'
-                auxObj[72] = 'Valor Redução'
+                auxObj[65] = '';
+                auxObj[66] = 'Ano';
+                auxObj[67] = 'Venda/Devolução';
+                auxObj[68] = 'Classificação';
+                auxObj[69] = 'Valor Isenção';
+                auxObj[70] = 'Valor Redução';
             } else if (rowIndex > 1) {
-                const sellOrDevolution = auxObj[56].startsWith('Venda') ? 'Venda' : 'Devolução'
-                const exemptionOrReduction = auxObj[57] == '040' ? 'Isenção' : 'Redução'
-                let exemptionValue = '';
-                let reductionValue = '';
+                const sellOrDevolution = auxObj[56] ? auxObj[56].startsWith('Venda') ? 'Venda' : 'Devolução' : '-';
+                const exemptionOrReduction = auxObj[57] ? auxObj[57] == '040' ? 'Isenção' : 'Redução' : '-';
+                let exemptionValue = 0;
+                let reductionValue = 0;
     
-                if (exemptionOrReduction == 'Isenção') {
-                    const exemption = auxObj[54] * 0.18
-                    exemptionValue = exemption.toFixed(2)
-                } else {
-                    const reduction = auxObj[64] * (auxObj[59] / 100)
-                    reductionValue = reduction.toFixed(2)
+                if (exemptionOrReduction != '-') {
+                    if (exemptionOrReduction == 'Isenção') {
+                        const exemption = auxObj[54] * 0.18;
+                        exemptionValue = exemption.toFixed(2);
+                    } else {
+                        const reduction = auxObj[64] * (auxObj[59] / 100);
+                        reductionValue = reduction.toFixed(2);
+                    }
                 }
-    
-                auxObj[68] = auxObj[2].split('/')[2]
-                auxObj[69] = sellOrDevolution
-                auxObj[70] = exemptionOrReduction
-                auxObj[71] = exemptionValue
-                auxObj[72] = reductionValue
+
+                auxObj[66] = auxObj[22] ? auxObj[22].split('/')[2] : '-'
+                auxObj[67] = sellOrDevolution
+                auxObj[68] = exemptionOrReduction
+                auxObj[69] = exemptionValue
+                auxObj[70] = reductionValue
             }
     
             rawObjs.push(auxObj)
